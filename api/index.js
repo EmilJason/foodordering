02393ec65759f5f99e6392e57e81e9fa1daeead7
EmilJason <emiljasondatuin@gmail.com>
2024-07-08 21:32:20 +0800

    add path module

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path')
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const foods = require('../data.js')
