c1b76ecd2b40225206f0470ca6114193c75eff9a
EmilJason <emiljasondatuin@gmail.com>
2024-07-08 17:01:56 +0800

    add static files

--- api/index.js
+++ api/index.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/foods', (req, res) => {
     try {
