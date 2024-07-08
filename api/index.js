57872705b3e05d41064441e9888d02980727688d
EmilJason <emiljasondatuin@gmail.com>
2024-07-08 16:46:06 +0800

    add api folder

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const foods = require('./data.js')
+const foods = require('../data.js')
 const cors = require('cors');
 const app = express();
 app.use(cors());
